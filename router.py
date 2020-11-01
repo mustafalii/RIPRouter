e339940b3792a700c25cbe13a924c42157b3c483
Mustafa Ali <mali13@cycle1.csug.rochester.edu>
2020-11-01 16:48:59 -0500

    fix network addr

--- router.py
+++ router.py
@@ -1,6 +1,7 @@
 # Class to define Router object
 # Router contains a count of interfaces, a list of interfaces, and a routing table
 from interface import Interface
+import re
 
 class Router:
     def __init__(self):
@@ -17,7 +18,8 @@ class Router:
         # update routing table
         destinationAddr = interface.ipAddr
         CIDR = interface.CIDR
-        dest_entry = destinationAddr + '/' + str(CIDR)  # @TODO: instead of the original ip, store only the network number e.g. instead of 192.168.3.1/24 we store 192.168.3.0/24
+        # instead of the original ip, store only the network number e.g. instead of 192.168.3.1/24 we store 192.168.3.0/24
+        dest_entry = calculateNetworkAddr(destinationAddr, CIDR) + '/' + str(CIDR) 
         nextHopMacAddr = 'FF:FF:FF:FF:FF:FF'
         nextHopInterface = interface.interfaceNum
         # example of an entry: {192.168.3.0/24: [1, FF:FF:FF:FF:FF:FF, 0]}
@@ -35,3 +37,17 @@ class Router:
             if interface.macAddr == macAddr:
                 return interface
         return None
+
+def calculateNetworkAddr(ipAddr, CIDR):
+    tokens = ipAddr.split('.')
+    networkBits = ""
+    for token in tokens:
+        binary = format(int(token), '08b') # formats segment into 8bit binary
+        networkBits += binary
+    networkBits = networkBits[0:int(CIDR)] + str('0' * (32-int(CIDR)))
+    bitGroups = re.findall("[01]{8}", networkBits)
+    networkAddr = ''
+    for group in bitGroups:
+        networkAddr = networkAddr + str(int(str(int(group)), 2)) + '.'
+    networkAddr = networkAddr[:len(networkAddr)-1]  # truncate last dot
+    return networkAddr
